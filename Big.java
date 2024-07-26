9a9d157a7e13dddeda9463a404b3f45418c00335
Sumit Bashetwar <84890708+Sumitbashetwar@users.noreply.github.com>
2024-07-26 19:27:48 +0530

    Add files via upload

---
+++ Big.java
@@ -0,0 +1,23 @@
+package DSA;
+
+
+import java.util.*;
+import java.util.concurrent.FutureTask;
+
+
+public class Big {
+    public static void main(String[] args) {
+        TreeSet<Integer> treeSet = new TreeSet<>(Collections.reverseOrder());
+        treeSet.add(10);
+        treeSet.add(112);
+        treeSet.add(13);
+        treeSet.add(1123);
+        treeSet.add(143);
+        treeSet.add(167);
+        treeSet.add(12);
+        treeSet.add(11);
+        treeSet.add(1);
+        System.out.println(treeSet);
+
+    }
+}
